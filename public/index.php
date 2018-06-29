c090ff2a10ed68d29fded4dee7e0a41afad52b4a
Carlos <cgl@dementecreativo.com>
2018-06-29 07:53:24 -0430

    News

--- public/index.php
+++ public/index.php
@@ -9,13 +9,19 @@
 define('DOC_ROOT', $_SERVER["DOCUMENT_ROOT"]);
 
 define('CORE_ROOT', str_replace("\\","/", dirname(__DIR__).'/'));
+
 define('SYSTEM_ROOT', CORE_ROOT."system/");
+
 define('APP_ROOT', CORE_ROOT."app/");
 
 
-define('REQUEST_URI', parse_url(rtrim($_SERVER["REQUEST_URI"],'/'), PHP_URL_PATH));
+$REQUEST_URI = parse_url(rtrim($_SERVER["REQUEST_URI"],'/'), PHP_URL_PATH);
+
+define('REQUEST_URI', ($REQUEST_URI == "") ? "/" : $REQUEST_URI);
+
 define('HTTP_ROOT', (isset($_SERVER['HTTPS']) ? 'https' : 'http') . "://".$_SERVER['HTTP_HOST']."/");
 
+/*
 echo "'DOCUMENT_ROOT' apunta a la carpeta del proyecto (virtualhost o servidor)<br><br>";
 
 echo "<b>DOC_ROOT:</b> ".DOC_ROOT." (raiz original)<br>";
@@ -25,3 +31,4 @@ echo "<b>CORE_ROOT:</b> ".CORE_ROOT." (para includes/requires del proyecto)<br>"
 echo "<b>REQUEST_URI:</b> ".REQUEST_URI." (para enrutar sin variables _GET)<br>";
 
 echo "<b>HTTP_ROOT:</b> ".HTTP_ROOT." (html -> base href='HTTP_ROOT')<br>";
+*/
